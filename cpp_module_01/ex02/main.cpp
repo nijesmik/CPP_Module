f9adf0658ff29b1024253347e1063f27ccda17f4
nijesmik <bkybb@duck.com>
2023-12-22 21:00:10 +0900

    feat(cpp01): ex02

---
+++ cpp_module_01/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(void) {
+	std::string str = "HI THIS IS BRAIN";
+	std::string *strPtr = &str;
+	std::string &strRef = str;
+
+	std::cout << "The memory address of the string variable : " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR : " << &strPtr << std::endl;
+	std::cout << "The memory address held by stringREF : " << &strRef << std::endl;
+	std::cout << std::endl;
+	std::cout << "The value of the string variable : " << str << std::endl;
+	std::cout << "The value pointed to by stringPTR : " << strPtr << std::endl;
+	std::cout << "The value pointed to by stringREF : " << strRef << std::endl;
+
+	return 0;
+}
