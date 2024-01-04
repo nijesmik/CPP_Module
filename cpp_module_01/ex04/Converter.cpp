6448ae66bf5516c6d659d1f100b4dd87c94d87f3
nijesmik <bkybb@duck.com>
2024-01-04 19:06:33 +0900

    fix(cpp01): ex04

--- cpp_module_01/ex04/Converter.cpp
+++ cpp_module_01/ex04/Converter.cpp
@@ -4,16 +4,21 @@ void Converter::convert(std::string fileName, std::string s1, std::string s2) {
 	std::ifstream input(fileName);
 	if (!input.is_open()) {
 		std::cerr << "File open failed!" << std::endl;
+		return;
 	}
 
 	std::string newFileName = fileName + ".replace";
 	std::ofstream output(newFileName, std::ios_base::trunc);
 	if (!output.is_open()) {
 		std::cerr << "Cannot create file!" << std::endl;
+		return;
 	}
 
 	std::string line;
 	while (std::getline(input, line)) {
+		if (!input.eof()) {
+			line += "\n";
+		}
 		size_t pos = line.find(s1);
 		while (pos != std::string::npos) {
 			line.erase(pos, s1.length());
@@ -21,9 +26,6 @@ void Converter::convert(std::string fileName, std::string s1, std::string s2) {
 			pos = line.find(s1, pos + s2.length());
 		}
 		output << line;
-		if (!input.eof()) {
-			output << std::endl;
-		}
 	}
 
 	input.close();
