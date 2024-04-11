2c49fde33511482c0997d3d070950936b92d195f
nijesmik <bkybb@duck.com>
2024-04-11 22:29:28 +0900

    feat(cpp02): ex01

---
+++ cpp-module-02/ex01/Fixed.cpp
@@ -0,0 +1,55 @@
+//
+// Created by 김세진 on 4/11/24.
+//
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : rawBit(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int value) : rawBit(value << this->fractionalBits) {
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float value) : rawBit(roundf(value * (1 << this->fractionalBits))) {
+    std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &fixed) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = fixed;
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed) {
+    std::cout << "Copy assignation operator called" << std::endl;
+    this->rawBit = fixed.getRawBits();
+    return *this;
+}
+
+int Fixed::getRawBits() const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->rawBit;
+}
+
+void Fixed::setRawBits(int const raw) {
+    std::cout << "setRawBits member function called" << std::endl;
+    this->rawBit = raw;
+}
+
+int Fixed::toInt() const {
+    return this->rawBit >> this->fractionalBits;
+}
+
+float Fixed::toFloat() const {
+    return (float) this->rawBit / (1 << this->fractionalBits);
+}
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fixed) {
+    os << fixed.toFloat();
+    return os;
+}
---
+++ cpp-module-02/ex01/Fixed.hpp
@@ -0,0 +1,41 @@
+//
+// Created by 김세진 on 4/11/24.
+//
+
+#ifndef CPP_MODULE_FIXED_HPP
+#define CPP_MODULE_FIXED_HPP
+
+#include "iostream"
+#include "cmath"
+
+class Fixed {
+private:
+    int rawBit;
+    static const int fractionalBits = 8;
+
+public:
+    Fixed();
+
+    Fixed(const int value);
+
+    Fixed(const float value);
+
+    Fixed(const Fixed &fixed);
+
+    ~Fixed();
+
+    Fixed &operator=(const Fixed &fixed);
+
+    int getRawBits() const;
+
+    void setRawBits(int const raw);
+
+
+    int toInt() const;
+
+    float toFloat() const;
+};
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fixed);
+
+#endif //CPP_MODULE_FIXED_HPP
