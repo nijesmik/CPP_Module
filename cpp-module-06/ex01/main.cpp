ea0e305f004f1b5c8a0e32ffbc61c58f4880bb41
nijesmik <bkybb@duck.com>
2024-07-08 20:19:20 +0900

    fix(cpp06): ex01
    - main문 수정

--- cpp-module-06/ex01/main.cpp
+++ cpp-module-06/ex01/main.cpp
@@ -14,7 +14,7 @@ Data newData(int date, std::string text) {
 }
 
 int main() {
-    Data data1 = newData(20240624, "Hello, World!");
+    Data data1 = newData(20240706, "Hello, World!");
 
     uintptr_t serializedData = Serializer::serialize(&data1);
     Data *data2 = Serializer::deserialize(serializedData);
@@ -27,6 +27,8 @@ int main() {
     std::cout << "serialized data(hex): " << ss.str() << std::endl;
 
     std::cout << "address of data2: " << data2 << std::endl;
+    std::cout << "data2->date: " << data2->date << std::endl;
+    std::cout << "data2->text: " << data2->text << std::endl;
 
     return 0;
 }
